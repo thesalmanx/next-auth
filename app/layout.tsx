5942fc7cd3e99e2dc21fac030ad886efa87a4036
salman <thesalmanx@gmail.com>
2024-12-06 20:02:26 +0500

    fixed google auth callback error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { SessionProvider } from 'next-auth/react'
-import { auth } from '@/auth'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Authentication System',
-  description: 'A simple authentication service',
-}
+  title: "Authentication System",
+  description: "An authentication system built with next-auth-v5",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const session = await auth();
 
   return (
     <SessionProvider session={session}>
       <html lang="en">
-        <body className={inter.className}>
+        <body
+          className={`flex flex-col min-h-screen items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 ${inter.className}`}
+        >
           <Toaster />
-          {children}
+          <div className="flex-grow w-full">{children}</div>
+          <p className="text-white text-center text-sm py-4">
+            Built from ♥️ by Salman
+          </p>
         </body>
       </html>
     </SessionProvider>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
         <h1 className={cn(
-          "text-6xl font-semibold text-white drop-shadow-md",
+          "text-5xl font-semibold text-white drop-shadow-md",
           font.className,
         )}>
           🔐 Auth
         </h1>
         <p className="text-white text-lg">
-          A simple authentication service
+            An authentication system built with Next.js
         </p>
         <div>
           <LoginButton  asChild>
